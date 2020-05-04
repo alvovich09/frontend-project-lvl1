65a47dcb4b16ca2da69acf4ee34c60706e766e95
alvovich <slaer2010@gmail.com>
2020-05-04 12:43:32 +0400

    fix 1st part of problem

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Hello, alvovich
-[![frontend-project1](https://github.com/alvovich09/frontend-project-lvl1/workflows/frontend-project1/badge.svg)]
+[![frontend-project1](https://github.com/alvovich09/frontend-project-lvl1/workflows/frontend-project1/badge.svg)] 
+[![Maintainability](https://api.codeclimate.com/v1/badges/df6b0cfdc4622ffe6bff/maintainability)](https://codeclimate.com/github/alvovich09/frontend-project-lvl1/maintainability)
 [![asciicast](https://asciinema.org/a/X5EqJWiWwSXipQPGYd6f8InZY.svg)](https://asciinema.org/a/X5EqJWiWwSXipQPGYd6f8InZY)
 [![asciicast](https://asciinema.org/a/W89iiaMbwe4svuArvbiEISY2P.svg)](https://asciinema.org/a/W89iiaMbwe4svuArvbiEISY2P)
 [![asciicast](https://asciinema.org/a/5UlpIL6cVwHfarSafoprxcsY4.svg)](https://asciinema.org/a/5UlpIL6cVwHfarSafoprxcsY4)
--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import readlineSync from 'readline-sync';
 
 const runGame = (getData, condition) => {
+  const round = 3; 
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(condition);
 
-  for (let i = 1; i <= 3; i += 1) {
+  for (let i = 1; i <= round; i += 1) {
     const [question, rightAnswer] = getData();
     console.log(`Question: ${question}`);
-    const userNumber = readlineSync.question('Your answer:');
-    if (userNumber === String(rightAnswer)) {
+    const userAnswer = readlineSync.question('Your answer:');
+    const correctAnswer = String(rightAnswer);
+    if (userAnswer === correctAnswer) {
       console.log('Correct!');
-      if (i === 3) {
+      if (i === round) {
         console.log(`Congratulations, ${userName}`);
       }
     } else {
-      console.log(`${userNumber} is wrong answer ;(. Correct answer was ${rightAnswer}.\nLet's try again, ${userName}!`);
-      break;
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.`)
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
   }
 };
